3b252a7173ec3f8d763d41c99c3956200712c067
keylighter <keysuggest@gmail.com>
2021-10-04 12:10:58 +0600

    Json file

---
+++ music.js
@@ -0,0 +1,75 @@
+const cources = [
+
+    {
+        id: 101,
+        name: 'Vocal Training ',
+        price: 1400,
+        duration: 36,
+        img: 'https://images.pexels.com/photos/4988132/pexels-photo-4988132.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great vocal , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 102,
+        name: 'Piano Trainig',
+        price: 1600,
+        duration: 56,
+        img: 'https://images.pexels.com/photos/4088819/pexels-photo-4088819.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great pianist , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 103,
+        name: 'Guitar Training',
+        price: 1500,
+        duration: 48,
+        img: 'https://images.pexels.com/photos/7715390/pexels-photo-7715390.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great guitarist , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 104,
+        name: 'Violin Trainig',
+        price: 1600,
+        duration: 72,
+        img: 'https://images.pexels.com/photos/6054109/pexels-photo-6054109.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great violin player , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 105,
+        name: 'Harmonica Training',
+        price: 1200,
+        duration: 48,
+        img: 'https://images.pexels.com/photos/9198426/pexels-photo-9198426.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great harmonica player , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 106,
+        name: 'Ukulele Training',
+        price: 1400,
+        duration: 72,
+        img: 'https://images.pexels.com/photos/4115012/pexels-photo-4115012.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great ukulele player , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 107,
+        name: 'Drums Training',
+        price: 1600,
+        duration: 96,
+        img: 'https://images.pexels.com/photos/4087991/pexels-photo-4087991.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great drummer , world is waiting for watching your hidden talent .'
+
+    },
+    {
+        id: 108,
+        name: 'Flute Training',
+        price: 1500,
+        duration: 96,
+        img: 'https://images.pexels.com/photos/6647480/pexels-photo-6647480.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        description: 'explore your music learning by learning how to be a great flute player , world is waiting for watching your hidden talent .'
+
+    }
+]
